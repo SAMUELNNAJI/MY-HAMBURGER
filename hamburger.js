5c735986cbfdca818f9cc5740f181750baebd596
SAMUELNNAJI <9jaflavzmedia@gmail.com>
2026-03-04 06:22:46 +0100

    done

--- hamburger.js
+++ hamburger.js
@@ -1,11 +1,35 @@
 const toggle = document.querySelector('.toggle');
 const hamburger = document.querySelector('nav');
+const pop = document.querySelector('#popup')
+const cancle = document.querySelector('.cancle')
 
 toggle.addEventListener('click', () => {
-    if (hamburger.style.display === 'flex') {
-        hamburger.style.display = 'none';
-
-    } else {
-        hamburger.style.display = 'flex';
-    }
+    hamburger.classList.toggle("active")
 })
+
+window.addEventListener('load', () => {
+    pop.classList.add("popActive")
+})
+
+cancle.addEventListener('click', () => {
+    pop.classList.remove("popActive")
+    //or pop.style.display = 'none'
+})
+
+
+
+// using if statement
+// toggle.addEventListener('click', () => {
+//     if (hamburger.style.display === 'flex') {
+//         hamburger.style.display = 'none'
+//     } else {
+//         hamburger.style.display = 'flex';
+//     }
+// })
+
+// hamburger.addEventListener('mouseleave', () => {
+//     hamburger.style.display = 'none'
+// })
+
+
+
